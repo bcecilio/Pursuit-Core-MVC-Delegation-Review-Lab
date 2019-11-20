893fe22717b655f1efc9fe96b3e269ad99a53981
brendon cecilio <brendon.cecilio@gmail.com>
2019-11-20 18:02:19 -0500

    slider and stepper functioning

--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/FontViewController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/FontViewController.swift
@@ -13,15 +13,35 @@ class FontViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var sliderStatus: UILabel!
     @IBOutlet weak var stepperFont: UIStepper!
+    @IBOutlet weak var stepperLabel: UILabel!
+    
 
     var currentFontSize: Int = 17 {
         didSet {
-            
+            stepperLabel.text = "Looking at \(stepperFont.value) Cohort."
         }
     }
     
+    override func viewDidLoad() {
+        configureStepper()
+        configureSlider()
+    }
+    
+    func configureStepper() {
+        stepperFont.minimumValue = 10
+        stepperFont.maximumValue = 36
+        stepperFont.stepValue = 1
+        stepperFont.value = 17
+    }
+    
+    func configureSlider() {
+        slider.minimumValue = 10
+        slider.maximumValue = 36
+        slider.value = 17
+    }
+    
     @IBAction func stepperFunction(_ sender: UIStepper) {
-        stepperFont.value = sender.value
+        currentFontSize = Int(stepperFont.value)
         slider.value = Float(sender.value)
         sliderStatus.text = Int(sender.value).description
     }
